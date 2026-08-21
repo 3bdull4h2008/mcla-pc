915f7d16c68077d095630c52615143a00eeb5f2e
3bdull4h <abdullahpowerfull@gmail.com>
2026-08-22 00:40:53 +0300

    repo hygiene: drop dead headers, one-off bats, log spam
    delete xdbf.h (broken include, zero refs), duplicate guest_stack_var.h twins, 8 tracked one-off bat scripts; untracked logs/.obj/.d artifacts removed from worktree

--- build_all_vs2026.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Use VS 2026 BuildTools for Windows SDK paths
-call "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x64 -host_arch=x64
-
-REM Configure
-cmake -S . -B build -G Ninja -DCMAKE_C_COMPILER=clang-cl -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_BUILD_TYPE=RelWithDebInfo
-
-REM Build
-cmake --build build
