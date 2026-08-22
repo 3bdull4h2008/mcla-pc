b1bacf2c61bacf6a47fe47fc09d020738a97591c
3bdull4h <abdullahpowerfull@gmail.com>
2026-08-23 00:22:38 +0300

    repo hygiene: remove stray flutter zip + temp script from last commit

--- tools/_b.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat" >nul 2>&1
-cmake --build build 2>&1 | findstr /i "error"
-echo BUILD_EXIT=%ERRORLEVEL%
